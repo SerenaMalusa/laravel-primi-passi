9bfe40397145d7f08462b74f9dccb02b81deb382
SerenaMalusa <sere.malu@gmail.com>
2024-03-14 17:44:05 +0100

    feat: add title trough variable

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('homepage');
+    $title = 'La mia homepage';
+    return view('homepage', [
+        'title' => $title
+    ]);
 });
